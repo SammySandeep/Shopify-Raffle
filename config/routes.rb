61ab3c529fc70ff305f61819f65951968b213638
dilip <dilip.kumar.m@kloctechnologies.co.in>
2020-06-29 11:19:09 +0530

    fix: 	-- refactored the routes.rb file 	-- removed unwanted routes

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,6 @@ Rails.application.routes.draw do
   get 'raffles', to: 'raffles#index'
   get 'raffles/:id', to: 'raffles#show', as: 'raffle'
 
-  get 'collections', to: 'collections#index'
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   post '/shopify_app/webhooks/products_info', to: 'shopify_app/webhooks#products_info'
 end
