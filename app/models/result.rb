f14ca77ce99f73f1ac610777ad3872f8a295097d
sandeep182 <s.sandeep@kloctechnologies.co.in>
2020-08-11 09:12:56 +0530

    feat: added default participate chance in result.rb file

--- app/models/result.rb
+++ app/models/result.rb
@@ -1,9 +1,17 @@
 # frozen_string_literal: true
 
 class Result < ApplicationRecord
+  before_save :reduce_customer_chance_by_one
 
   belongs_to :customer
   belongs_to :raffle
   has_one :notification
 
+  private
+
+  def reduce_customer_chance_by_one
+    customer = Customer.find(self.customer_id)
+    customer.default_participant_chance -= 1
+    customer.save
+  end
 end
