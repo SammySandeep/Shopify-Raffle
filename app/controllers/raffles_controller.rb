a8af07552ed425b57454fff339d4d60a83b46f3a
shreya <shreya.b@kloctechnologies.co.in>
2020-09-23 15:54:26 +0530

    feat: validation for mail to participants

--- app/controllers/raffles_controller.rb
+++ app/controllers/raffles_controller.rb
@@ -58,8 +58,12 @@ class RafflesController < HomeController
     shop = shop session
     results_participant = take_results_by_raffle_id_and_type_of_customer raffle.id, 'participant'
     participant_customers = take_customers results_participant
-    Raffle.send_mail_for_participants raffle, participant_customers, shop 
-    redirect_to winner_and_runner_customers_path(raffle.id), notice: 'Mail Triggered Successfully to all Participants!'
+    if participant_customers.nil?
+      redirect_to raffles_path, notice: 'No participants found for this raffle!'
+    else
+      Raffle.send_mail_for_participants raffle, participant_customers, shop 
+      redirect_to winner_and_runner_customers_path(raffle.id), notice: 'Mail Triggered Successfully to all Participants!'
+    end
   end
 
   private
