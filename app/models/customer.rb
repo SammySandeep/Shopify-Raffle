598d26b0aa96867b54a864be8e35abe2e18ffdb8
sandeep182 <s.sandeep@kloctechnologies.co.in>
2020-10-29 14:44:46 +0530

    chore: removed validations for customers

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -9,10 +9,6 @@ class Customer < ApplicationRecord
   has_one :address
 
   validates :shopify_customer_id, numericality: { greater_than: 0 }, presence: true
-  validates :first_name, presence: true
-  validates :first_name, format: { with: /[a-zA-Z]/ }
-  validates :last_name, presence: true
-  validates :last_name, format: { with: /[a-zA-Z]/ }
   validates :email_id, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, length: { maximum: 50 }, presence: true
   validates :default_participant_chance, numericality: { greater_than_or_equal_to: -1 }
   validates :shop_id, numericality: { greater_than: 0 }, presence: true
